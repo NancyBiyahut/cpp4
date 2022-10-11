87b37e56f703f09e1dc5d351cb6a411022495e56
NancyBiyahut <99260268+NancyBiyahut@users.noreply.github.com>
2022-10-11 22:17:27 +0545

    Create resolve.cpp

---
+++ resolve.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* root)
+    {
+      //Your code here
+      queue<Node *>q;
+      
+      vector<int>ans;
+      
+      if(root==NULL)
+      {
+          return ans;
+      }
+      q.push(root);
+      while(!q.empty())
+      {
+          Node*temp=q.front();
+          q.pop();
+          ans.push_back(temp->data);
+          if(temp->left!=NULL)
+          {
+              q.push(temp->left);
+          }
+          if(temp->right!=NULL)
+          {
+              q.push(temp->right);
+          }
+          
+      }
+      
+      return ans;
+      
+ }
+};
